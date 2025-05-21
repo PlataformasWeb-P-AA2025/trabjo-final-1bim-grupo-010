7cfce01145014438145883327bddfc61631ff155
edisson2407 <chambaedisson01@gmail.com>
2025-05-21 09:55:20 -0500

    cambio3

--- consulta1.py
+++ consulta1.py
@@ -1,5 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import and_ 
+from genera_tablas import *
 from configuracion import cadena_base_datos
-
 engine = create_engine(cadena_base_datos)
 
 
@@ -9,5 +12,7 @@ session = Session()
 
 #Listar publicaciones de un usuario.
 
-usuarios = session.query(Usuario).filter(Usuario.nombre.like("%Shelley%")).all()
-print(usuarios)
+usuarios = session.query(Publicacion).join(Usuario).filter(Usuario.usuario.like("%Karen%")).all()  #usamos join para unir publicaciones con usuario y filtramos para especificar que usuario mostrar
+
+for l in usuarios:
+	print(f"{l.publicacion}") #recorremos para listar todas las publicaciones del usuario especificado
