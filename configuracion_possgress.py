de0585e663938b98c4d374f6c404288fdbd074c8
bmvaldivieso <bmvaldivieso@utpl.edu.ec>
2025-06-03 09:29:28 -0500

    cambio 6

---
+++ configuracion_possgress.py
@@ -0,0 +1,11 @@
+# este módulo será usado para posibles configuraciones
+#
+# cadena conector a la base de datos
+#
+# Sqlite
+cadena_base_datos = 'postgresql+psycopg2://postgres:tu_contraseña@localhost:5432/postgres'
+
+# Mysql
+# para el uso de este dialecto en SqlAlchemy
+# instalar "pip install PyMySQL"
+# cadena_base_datos = 'mysql+pymysql://usuario:clave@localhost/bd_name'
